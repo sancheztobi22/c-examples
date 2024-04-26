cbb8a1dfcdc5853083f359f3526453bd95a7d4d9
tobiassanchez <tobias.sanchez@ucr.ac.cr>
2024-04-26 16:14:07 -0600

    cambios en main

--- funciones/9-encabezados.c
+++ funciones/9-encabezados.c
@@ -4,14 +4,21 @@
 int main() {
 
     int v;
+    int z;
 
     // Llama a las 3 funciones
     funcion1();
     funcion2(5, 2.67);
     v = funcion3(10);
+    z = funcion4(4);
 
     // Imprime el valor que retornó funcion3()
     printf("v = %d\n", v);
+
+    /// imprimir el valor que retorno funcion4()
+    ///
+    printf("z = %d\n", z);
+
 
     return 0;
 
@@ -35,3 +42,12 @@ int funcion3(int x) {
     int y = x + 2;
     return y;
 }
+
+/// implementacion de funcion4 para la resta
+///
+int funcion4(int x){
+	printf("En funcion4() ...\n");
+        int z = x - 2;
+	return z;
+}
+
